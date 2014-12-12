0d64b2ec823a5acd281ecb4e7df7ff0b61006bee
atishaybaid <atishaybaid@gmail.com>
2014-12-12 15:10:48 +0530

    routing done

--- timely/js/app.js
+++ timely/js/app.js
@@ -1,1 +1,19 @@
-var timelyApp = angular.module("timelyApp",[]);
+var timelyApp = angular.module("timelyApp",['ngRoute']);
+
+timelyApp.config(['$routeProvider',function($routeProvider){
+	console.log("inside routeProvider");
+	$routeProvider.
+	when('/home',{
+		//console.log("inside home");
+		templateUrl:'js/partials/home.html',
+		controller:'mainCtrl'
+	}).
+	when('/setting',{
+		templateUrl:'js/partials/settingScreen.html',
+		controller:'mainCtrl'
+	}).
+	otherwise({
+		redirectTo:'/home'
+	});
+
+}]);
