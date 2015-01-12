75407655540d3c0631bec31580f2e92882158b6f
atishaybaid <atishaybaid@gmail.com>
2015-01-12 17:09:10 +0530

    directive demo

--- directiveDemoApp/app.js
+++ directiveDemoApp/app.js
@@ -2,11 +2,22 @@ var directiveApp = angular.module("directiveApp",[]);
 
 var mainCtrl = directiveApp.controller("mainCtrl",function($scope){
 
+	$scope.check =  0;
+
 	$scope.displayData = function(){
 		console.log("inside displayData");
 
 	};
 
+	$scope.keyPress = function(keyCode){
+   console.log(keyCode); 
+	};
+
+	$scope.data = [1,2,3];
+	
+
+	
+	
 	
 }); 
 
@@ -15,17 +26,47 @@ directiveApp.directive("populateList",function(){
 
 //var tasks =[];
 
-	return{
-		scope:{
-			show:'&'
-		},
-		
+	return{		
+		compile:function(element,attribute){
+			var elementTmpl = angular.element('<div id="searchBox"><input type="text" name="" id="">'+
+								'<ul ng-show="data">'+
+								'<li ng-repeat="data in data">{{data}}</li>'+
+								'</ul></div>');
+			
+
+			element.replaceWith(elementTmpl);
+			var input = elementTmpl.find('input');
+			
+			return function(scope,element,attribute){
+				console.log("inside link");		
+				var minKeys = 3;
+				input.bind('keyup',function(e){
+					console.log("inside bind");
+					value = input.val();
+					console.log(value.length);
+					console.log(scope.data);
+				});
+
+			}
 
 
-		template:'<input type="text" name="" id="">' +
-		          '<input type="button" value="Add" ng-click="show()">'
+
+
+
+
+
+
+
+
+			
+
+
+	
+
+		          
 
 
 	}
+		}
 
 });
