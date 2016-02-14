77e8515783d7b0c727864df6ad19efcebf1dedec
Atishay <atishaybaid@gmail.com>
2016-02-14 23:34:37 +0530

    insertion Sort implemneted

--- dsLearning/insertionSort.js
+++ dsLearning/insertionSort.js
@@ -1,14 +1,16 @@
 function insertionSort(arr) {
-    for (i = 1; i < arr.length; i++) {
-        var key = arr[i];
-        var j = i - 1;
-
-        while (arr[j] > key && j >= 0) {
-            arr[j + 1] = arr[j];
-            j--;
+    var i,j,temp;
+    for(j=1;j<arr.length+1;j++){
+        for(i = j;i>=0;i--){
+            if(arr[i]<arr[i-1]){
+                temp = arr[i-1];
+                arr[i-1] = arr[i];
+                arr[i] = temp;
+            }
         }
-        arr[j + 1] = key;
     }
+
+
 
 }
 
